5bf45e50ef8128f3c332f92bf3bc4821378a615f
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-24 23:17:21 -0300

    feat(setup): vue create

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
