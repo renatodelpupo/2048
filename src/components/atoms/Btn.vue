1123d4bfb52775a0d03096774a0696bc90bbc1c9
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-09-08 15:27:42 -0300

    feat: create button component

---
+++ src/components/atoms/Btn.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    class="Button"
+    :style="{
+      backgroundColor: style.backgroundColor,
+      color: style.textColor
+    }"
+    v-text="text"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Button',
+
+  props: {
+    text: {
+      default: '',
+      type: String
+    },
+    style: {
+      type: Object
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.Button {
+  border: none;
+  border-radius: 5px;
+  padding: 10px 15px;
+}
+</style>
