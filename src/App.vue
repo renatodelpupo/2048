8fa3dc82313306808c7c2be11fac48512fafdd87
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-09-08 15:26:57 -0300

    feat: set router view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <div id="app">
-    <Home />
-  </div>
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Home from './views/Home.vue'
 
 export default defineComponent({
-  name: 'App',
-
-  components: { Home }
+  name: 'App'
 })
 </script>
 
