82edadaeefbc91bb5eee461bab5d30ccb89691c1
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-09-08 15:28:53 -0300

    feat: create settings page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Settings from '../views/Settings.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
   }
 ]
 
---
+++ src/views/Settings.vue
@@ -0,0 +1,59 @@
+<template>
+  <generic-screen
+    :buttons="[
+      {
+        text: 'Restart'
+      },
+      {
+        event: 'toggle-theme',
+        text: 'Toggle theme'
+      }
+    ]"
+    title="Settings"
+    :style="style"
+    @toggle-theme="() => $emit('toggle-theme')"
+  />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import GenericScreen from '../components/templates/GenericScreen.vue'
+
+export default defineComponent({
+  name: 'Settings',
+
+  components: { GenericScreen },
+
+  props: {
+    darkTheme: {
+      default: true,
+      type: Boolean
+    }
+  },
+
+  setup(props) {
+    const style = computed(() => {
+      const dark = {
+        backgroundColor: '#1e1e1e',
+        button: {
+          backgroundColor: '#383838',
+          textColor: '#ffffff'
+        },
+        textColor: '#ffffff'
+      }
+      const light = {
+        backgroundColor: '#fffeef',
+        button: {
+          backgroundColor: '#7d95a9',
+          textColor: '#fffeef'
+        },
+        textColor: '#65839b'
+      }
+
+      return props.darkTheme ? dark : light
+    })
+
+    return { style }
+  }
+})
+</script>
